8c9b042c8f04a208a01485c8f50705ec77ebff30
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-05-14 18:43:21 +0530

    Updating finally resolved resetting the form.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-const crudCrudUrl = "https://crudcrud.com/api/25df059a0e9e4599bbfa91ea31761027/candies";
+const crudCrudUrl = "https://crudcrud.com/api/bbad9ced22e947a6867ea644647d0ab1/candies";
 
 
 function fetchAndDisplayCandies() {
@@ -27,41 +27,60 @@ function saveChoco(event) {
   let quantity = event.target.quantity.value;
   let candy = { candyName: candyName, desc: desc, price: price, quantity: quantity };
 
+
+
+
   axios.post(crudCrudUrl, candy)
     .then((res) => {
       console.log(res.data);
       fetchAndDisplayCandies(); // Fetch and display updated list
     })
     .catch((err) => console.log(err));
+
+    document.querySelector("#formData").reset();
 }
 
 function displayChoco(candy) {
+
+  if(!candy.candyName || !candy.desc || !candy.price || !candy.quantity){
+    return;
+  }
+  
   let ul = document.querySelector("#chocoList");
   const li = document.createElement("li");
   ul.appendChild(li);
   li.innerHTML = ` ${candy.candyName} - ${candy.desc} - ${candy.price} - <span>${candy.quantity}</span>`;
 
+
   const buyOneBtn = document.createElement("button");
   buyOneBtn.textContent = "Buy One";
-  buyOneBtn.addEventListener("click", () => buyCandy(candy._id, 1, candy.quantity));
+  buyOneBtn.addEventListener("click", () => buyCandy(candy._id,  candy.quantity-1));
   li.appendChild(buyOneBtn);
 
   const buyTwoBtn = document.createElement("button");
   buyTwoBtn.textContent = "Buy Two";
-  buyTwoBtn.addEventListener("click", () => buyCandy(candy._id, 2, candy.quantity));
+  buyTwoBtn.addEventListener("click", () => buyCandy(candy._id, candy.quantity-2));
   li.appendChild(buyTwoBtn);
 
   const buyThreeBtn = document.createElement("button");
   buyThreeBtn.textContent = "Buy Three";
 
-  buyThreeBtn.addEventListener("click", () => buyCandy(candy._id, 3, candy.quantity));
+  buyThreeBtn.addEventListener("click", () => buyCandy(candy._id,  candy.quantity-3));
   li.appendChild(buyThreeBtn);
 }
 
-function buyCandy(candyId, quantity, currentQuantity) {
-  const newQuantity = currentQuantity - quantity;
-  if (newQuantity < 0) {
+function buyCandy(candyId, updatedQuantity) {
+
+  if(updatedQuantity<0){
     alert("Not enough candies in stock");
+    return
+  }
+
+  if (updatedQuantity == 0) {
+    
+    axios.delete(`${crudCrudUrl}/${candyId}`)
+    fetchAndDisplayCandies()
+
     return
   }
 
@@ -70,13 +89,13 @@ function buyCandy(candyId, quantity, currentQuantity) {
 
       let { _id, ...candy } = res.data;
       //const candy = res.data;
-      // console.log(candy)
+       console.log(candy)
       if (!candy) {
         console.log(`Candy not found with ID ${candyId}`);
         return;
       }
 
-      const updatedCandy = { ...candy, quantity: newQuantity };
+      const updatedCandy = { ...candy, quantity: updatedQuantity };
       return axios.put(`${crudCrudUrl}/${candyId}`, updatedCandy)
         .then((res) => {
           console.log(res.data);
@@ -87,6 +106,5 @@ function buyCandy(candyId, quantity, currentQuantity) {
   }
 
 
-// 
 
 
